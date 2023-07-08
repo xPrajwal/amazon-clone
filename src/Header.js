2420729dfe2884b835c415d20960acdc6f34b5a2
Prajwal <meisprajwal@gmail.com>
2023-07-08 09:45:20 -0700

    feat: added basket functionality

--- src/Header.js
+++ src/Header.js
@@ -3,15 +3,20 @@ import React from "react";
 import "./Header.css";
 import SearchIcon from '@mui/icons-material/Search';
 import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
-function Header() {  
+import { Link } from "react-router-dom";
+import { useStateValue } from "./StateProvider";
+
+function Header() {
+  const [{ basket, user }, dispatch] = useStateValue();
+
   return (
     <div className="header">
-      
+      <Link to="/">
         <img
           className="header__logo"
           src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
         />
-      
+      </Link>
 
       <div className="header__search">
         <input className="header__searchInput" type="text" />
@@ -22,7 +27,7 @@ function Header() {
         
           <div className="header__option">
             <span className="header__optionLineOne">Hello Guest</span>
-            <span className="header__optionLineTwo">Sign In</span>
+            <span className="header__optionLineTwo">Sign in</span>
           </div>
         
 
@@ -39,14 +44,14 @@ function Header() {
           <span className="header__optionLineTwo">Prime</span>
         </div>
 
-        
+        <Link to="/checkout">
           <div className="header__optionBasket">
             <ShoppingBasketIcon />
             <span className="header__optionLineTwo header__basketCount">
-              0
+              {basket?.length}
             </span>
           </div>
-        
+        </Link>
       </div>
     </div>
   );
