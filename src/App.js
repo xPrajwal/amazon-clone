cdef346c93e42d6d0077960917a2cc34302915a2
Prajwal <meisprajwal@gmail.com>
2023-07-08 08:36:41 -0700

    feat: added nav bar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./Header";
 import Home from "./Home";
 function App() {
 return (
   <div className="app">
     <Router>
       <Routes>
+        <Route path="/" element={<Header />} />
         <Route path="/" element={<Home />} />
-      
       </Routes>
     </Router>
   </div>
